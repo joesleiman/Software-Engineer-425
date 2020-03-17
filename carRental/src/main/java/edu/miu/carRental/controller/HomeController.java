fa5720c1ef156bb7ff1ea53d1a8d0f95b8774688
Namuun <s.namuun88@gmail.com>
2020-03-16 21:43:20 -0500

    Booking added

---
+++ carRental/src/main/java/edu/miu/carRental/controller/HomeController.java
@@ -0,0 +1,97 @@
+package edu.miu.carRental.controller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.miu.carRental.domain.Booking;
+import edu.miu.carRental.domain.Car;
+import edu.miu.carRental.serviceImp.BookingServiceImp;
+import edu.miu.carRental.serviceImp.CarServiceImp;
+
+@RestController
+public class HomeController {
+	
+	@Autowired
+    private CarServiceImp carService;
+	
+	@Autowired
+	private BookingServiceImp bookingService;
+	
+	//@PreAuthorize("hasAnyRole('USER')")
+    @GetMapping("/cars")
+    public List<Car> getAllCars() {
+        return carService.findAll();
+    }
+    
+    //@PreAuthorize("hasAnyRole('USER')")
+    @GetMapping("/cars/{id}")
+    public Car getCar(@PathVariable Long id){
+        Car car= carService.findById(id);
+        return car;
+    }
+    
+    //@PreAuthorize("hasAnyRole('USER')")
+    @GetMapping("/search_cars/{id}")
+    public Car searchCars(@PathVariable Long id){
+        Car car= carService.findById(id);
+        return car;
+    }
+    
+    //@PreAuthorize("hasAnyRole('USER')")
+  	@PostMapping("/booking")
+  	public Booking addBooking(@RequestBody Booking booking) {
+  		return bookingService.save(booking);
+  	}
+    
+    //@PreAuthorize("hasAnyRole('USER')")
+  	@GetMapping("/search_booking/{referenceNumber}")
+  	public List<Booking> searchBooking(@PathVariable String referenceNumber){
+  		return bookingService.searchBookings(referenceNumber);
+  	}	
+    
+    //@PreAuthorize("hasAnyRole('USER')")
+  	@PutMapping("/cancel_booking/{referenceNumber}")
+  	public Booking cancelBooking(@PathVariable String referenceNumber) {
+  		return bookingService.customerCancelBooking(referenceNumber, "Canceled");
+  	}
+  	
+    //@PreAuthorize("hasAnyRole('USER')")
+  	@GetMapping("/booking/{referenceNumber}")
+  	public Booking getBooking(@PathVariable String referenceNumber) {
+  		//if booking is not found, then method will return null
+  		return bookingService.findByReferenceNumber(referenceNumber);
+  	}
+  	
+    //@PreAuthorize("hasAnyRole('USER')")
+  	@GetMapping("/check_availiable_cars")
+  	public List<Car> getAvailableCars(@RequestParam String start, String end){
+  		List<Car> cars = carService.findAll().stream()
+  				.filter(car->car.getCarStatus().equals("available")).collect(Collectors.toList());
+  		LocalDate startDate = LocalDate.parse(start, DateTimeFormatter.ISO_DATE);
+  		LocalDate endDate = LocalDate.parse(end, DateTimeFormatter.ISO_DATE);
+  		List<Booking> bookings = bookingService.getBookingsWithinRange(startDate, endDate);  		
+  		if(!bookings.isEmpty()) {
+  			List<Long> carIds = new ArrayList<>();
+  			for(Booking b: bookings){
+  				carIds.add(b.getCar().getCarId());
+  			}
+  			cars = cars.stream().filter(car->!carIds.contains(car.getCarId())).collect(Collectors.toList());
+  		}  
+  		
+  		return cars;
+  	}
+	
+}
--- carRental/src/main/java/edu/miu/carRental/domain/Booking.java
+++ carRental/src/main/java/edu/miu/carRental/domain/Booking.java
@@ -1,5 +1,7 @@
 package edu.miu.carRental.domain;
 
+import java.io.Serializable;
+import java.sql.Date;
 import java.time.LocalDate;
 
 import javax.persistence.CascadeType;
@@ -8,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -17,55 +20,75 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="bookings")
-public class Booking {
+public class Booking implements Serializable{
 	
 	@Id
 	@Column(name = "booking_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long bookingId;
 	
-	@Column(name = "reference_number")
+	@Column(name = "reference_number", nullable = false, unique = true)
 	@NotNull(message = "*Please provide reference number")
 	private String referenceNumber;
 	
-	@Column(name = "booking_date")
+	@Column(name = "booking_date", nullable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "*Please provide booking date")
 	private LocalDate bookingDate;
 
-	@Column(name = "start_date")
+	@Column(name = "start_date", nullable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "*Please provide booking start date")
     private LocalDate startDate;
 
-	@Column(name = "end_date")
+	@Column(name = "end_date", nullable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "*Please provide booking end date")
     private LocalDate endDate;
 	
-	@Column(name = "total_price")
+	@Column(name = "total_price", nullable = false)
 	@NotNull(message = "*Please provide total price")
 	private Double totalPrice;
 	
-	@Column(name = "booking_status")
+	@Column(name = "booking_status", nullable = false)
 	@NotNull(message = "*Please provide booking status")
 	private String bookingStatus;
 	
-	@ManyToOne(cascade = CascadeType.ALL)
-	//@Column(name = "car_id", nullable = false)
+	//@ManyToOne(cascade = CascadeType.ALL)
+	@ManyToOne
+	@JoinColumn(name = "car_id", nullable = false)
 	private Car car;
 	
-	@OneToOne(cascade = CascadeType.ALL)
-	//@Column(name="payment_id", nullable = false, unique = true)
+	//@OneToOne(cascade = CascadeType.ALL)
+	@OneToOne
+	@JoinColumn(name="payment_id", nullable = false, unique = true)
 	private Payment payment;
 	
-	@ManyToOne(cascade = CascadeType.ALL)
-	//@Column(name="customer_id", nullable = false)
+	//@ManyToOne(cascade = CascadeType.ALL)
+	@ManyToOne
+	@JoinColumn(name="customer_id", nullable = false)
 	private Customer customer;
 
     public Booking() {
     	
     }
+    
+    public Booking(@NotNull String referenceNumber,
+			@NotNull(message = "*Please provide booking date") LocalDate bookingDate,
+			@NotNull(message = "*Please provide booking start date and time") LocalDate startDate,
+			@NotNull(message = "*Please provide booking end date and time") LocalDate endDate, 
+			Double totalPrice, String bookingStatus,
+			Car car, Payment payment, Customer customer) {
+		this.referenceNumber = referenceNumber;
+		this.bookingDate = bookingDate;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.totalPrice = totalPrice;
+		this.car = car;
+		this.payment = payment;
+		this.customer = customer;
+		this.bookingStatus = bookingStatus;
+	}
 	
 	public String getBookingStatus() {
 		return bookingStatus;
@@ -96,27 +119,33 @@ public class Booking {
 	}
 
 	public LocalDate getBookingDate() {
-		return bookingDate;
+		return this.bookingDate;
+		//return Date.valueOf(bookingDate).toLocalDate().plusDays(1);
 	}
 
 	public void setBookingDate(LocalDate bookingDate) {
 		this.bookingDate = bookingDate;
+		//this.bookingDate = Date.valueOf(bookingDate).toLocalDate().plusDays(1);
 	}
 
 	public LocalDate getStartDate() {
 		return startDate;
+		//return Date.valueOf(startDate).toLocalDate().plusDays(1);
 	}
 
 	public void setStartDate(LocalDate startDate) {
 		this.startDate = startDate;
+		//this.startDate = Date.valueOf(startDate).toLocalDate().plusDays(1);
 	}
 
 	public LocalDate getEndDate() {
 		return endDate;
+		//return Date.valueOf(endDate).toLocalDate().plusDays(1);
 	}
 
 	public void setEndDate(LocalDate endDate) {
 		this.endDate = endDate;
+		//this.endDate = Date.valueOf(endDate).toLocalDate().plusDays(1);
 	}
 
 	public Double getTotalPrice() {
